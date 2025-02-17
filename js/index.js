3e98c0bdb3be6cf10a27c64b9b821c9309a38b4b
Dokey <asgerjd@gmail.com>
2025-02-17 15:22:34 +0100

    asger - fixed issues with categories not properly filtered

--- js/index.js
+++ js/index.js
@@ -1,31 +1,37 @@
-// let category_container = document.querySelector(".category_container");
+// const categoryContainer = document.querySelector(".category_container");
+// let url = `https://dummyjson.com/products/categories`;
 
-// fetch(`https://dummyjson.com/products/categories`)
-//     .then(response => response.json())
-//     .then((data) => showCategories(data));
+// fetch(url)
+//   .then((response) => response.json())
+//   .then((data) => showCategories(data));
 
-// function showCategories(categories) { 
-//     console.log(categories);
+// function showCategories(categories) {
+//   console.log(categories);
 
-//     // Filtrer kun de kategorier, du vil bruge
-//     let filteredCategories = categories.filter(cat => 
-//         cat === "beauty" || 
-//         cat === "fragrances" || 
-//         cat === "nail couture"
-//     );
+//   // Filtrer kun de kategorier, du vil bruge
+//   let filteredCategories = categories.filter((cat) => cat === "beauty" || cat === "fragrances" || cat === "nail couture");
 
-//     // Generer HTML til hver kategori:
-//     const markup = filteredCategories.map(
-//         (category) => `
+//   // Generer HTML til hver kategori:
+//   const markup = filteredCategories
+//     .map(
+//       (element) => `<div class="category_container">
 //             <div class="category-box">
-//             <a href="list.html"><img src="images/index-makeup.svg" alt="makeup"></a>
-//             </div>
-//         `
-//     ).join("");
+//             <a href="list.html?category=beauty"><img src="images/index-makeup.svg" alt="makeup" /></a>
+//           </div>
+//           <div class="category-box">
+//             <a href="list.html?category=fragrances"><img src="images/index-frag-her.svg" alt="frag-her" /></a>
+//           </div>
+//           <div class="category-box">
+//             <a href="list.html?category=fragrances"><img src="images/index-frag-men.svg" alt="frag-him" /></a>
+//           </div>
+//           <div class="category-box">
+//             <a href="list.html?category=nailcouture"><img src="images/index-nailpolish.svg" alt="nails" /></a>
+//           </div>`
+//     )
+//     .join("");
 
-//     // Indsæt den genererede HTML i containeren
-//     category_container.innerHTML = markup;
+//   categoryContainer.innerHTML = ``;
+
+//   // Indsæt den genererede HTML i containeren
+//   categoryContainer.innerHTML = markup;
 // }
-
-
-
